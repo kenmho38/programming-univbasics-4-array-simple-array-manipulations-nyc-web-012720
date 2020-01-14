19875d3b9ed7497f8b6ceee74de48b9ef0db231d
Ken Ho <kenmho38@hotmail.com>
2020-01-14 20:49:21 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,65 +1,47 @@
-def using_unshift(neighborhoods_in_northwest_brooklyn, new_neighborhood)
-  neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
-  new_neighborhood = "Staten Island"
-  neighborhoods_in_northwest_brooklyn.unshift(new_neighborhood)
+def using_push(array, string)
+  array.push(string)
 end
 
-
-def using_pop(continents)
-  continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
-  continents.pop
+def using_unshift(array, string)
+  array.unshift(string)
 end
 
-def pop_with_args(dog_breeds)
-  dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
-  dog_breeds.pop(2)
+def using_pop(array)
+  array.pop
 end
 
-def using_shift(my_favorite_cities)
-  my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
-  im_so_over_this_city = my_favorite_cities.shift
+def pop_with_args(array)
+  array.pop(2)
 end
 
-def shift_with_args(ice_cream_brands)
-  ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
-  brands_removed = ice_cream_brands.shift(2)
+def using_shift(array)
+  array.shift
 end
 
-def using_concat(my_favorite_things, more_favs)
-  my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
-  more_favs = ["sports cars", "flatiron school"]
-  all_my_favs = my_favorite_things.concat(more_favs)
+def shift_with_args(array)
+  array.shift(2)
 end
 
-def using_insert(list_of_programming_languages, another_esoteric_language)
-      list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
-   another_language = "Python"
-   new_array =list_of_programming_languages.insert(4,  another_language)
+def using_concat(array1, array2)
+  array1.concat(array2)
 end
 
-def using_uniq(haircuts)
-  haircuts = ["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler", "Bob"]
-  new_array = haircuts.uniq
+def using_insert(array, item)
+  array.insert(4, item)
 end
 
-def using_flatten(instruments)
-  instruments = ["Saxophone", ["Piano", "Trumpet"], "Violin", "Drums", "Flute"]
-  flat_array = instruments.flatten
+def using_uniq(array)
+  array.uniq 
 end
 
-def using_delete(my_list, my_element)
-  my_list.delete(my_element)
+def using_flatten(array)
+  array.flatten 
 end
 
-def using_delete_at(famous_robots, integer)
-  famous_robots = ["Johnny 5", "R2D2", "Robocop"]
-  integer = 2
-  deleted_robot = famous_robots.delete_at(integer)
+def using_delete(array, string)
+  array.delete(string)
 end
 
-
-
-
-
-
-
+def using_delete_at(array, num)
+  array.delete_at(num)
+end
